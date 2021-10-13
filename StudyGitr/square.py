28b4bc8f95f48a6849d2c13813f3b07c152da268
Alex <Alex@IT2-MQBKH7>
2021-10-13 17:32:12 +0300

    Merge branch 'master' of https://github.com/LexDUA/StudyGit.git

--- StudyGitr/square.py
+++ StudyGitr/square.py
@@ -3,12 +3,11 @@ Created on 13 окт. 2021 г.
 
 @author: Alex
 '''
-from my_funcs import square, cube
-
+import my_funcs as mf
 # for too funcs - loop from 1 to 10
 print("square")
 for i in range(10):
-    print(f"The square of {i} is {square(i)}")
+    print(f"The square of {i} is {mf.square(i)}")
 print("cube")    
 for i in range(10):
-    print(f"The square of {i} is {cube(i)}")
+    print(f"The square of {i} is {mf.cube(i)}")
