cb302a624a741d3b493da78ccfd70f51cd52d041
Aleksandr Dolbyshev <92167946+LexDUA@users.noreply.github.com>
2021-10-13 17:31:07 +0300

    Update square.py

--- StudyGitr/square.py
+++ StudyGitr/square.py
@@ -4,8 +4,9 @@ Created on 13 окт. 2021 г.
 @author: Alex
 '''
 from my_funcs import square, cube
-
+#
 # for too funcs - loop from 1 to 10
+#
 print("square")
 for i in range(10):
     print(f"The square of {i} is {square(i)}")
